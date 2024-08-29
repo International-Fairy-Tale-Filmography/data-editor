c0f1c92eb30bb8e1c40b3a972c67288268ab7f68
Jeff Lefebvre <thestamp@gmail.com>
2024-08-29 01:33:55 -0500

    added support for loading and saving files

--- DataEditor.Core/Services/GitService.cs
+++ DataEditor.Core/Services/GitService.cs
@@ -1,6 +1,9 @@
-﻿using Octokit;
+﻿using DataEditor.Core.Configuration;
+using Octokit;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,23 +12,51 @@ namespace DataEditor.Core.Services
     //https://www.daveabrock.com/2021/03/14/upload-files-to-github-repository/
     public class GitService
     {
-        private GitHubClient gitHubClient;
-        public GitService()
+        private readonly GitHubClient _gitHubClient;
+        public CoreSettingsModel _coreSettings;
+        public GitService(CoreSettingsModel settings)
         {
-            gitHubClient = new GitHubClient(new ProductHeaderValue("MyCoolApp"));
+            _coreSettings = settings;
+            _gitHubClient = new GitHubClient(new ProductHeaderValue("MyCoolApp"));
         }
 
-        public void SetCredentials(string accessToken)
+        
+
+        public void ApplyConfiguration()
         {
-            gitHubClient.Credentials = new Credentials(accessToken);
+            _gitHubClient.Credentials = new Credentials(_coreSettings.AccessToken);
         }
 
         public async Task<string> GetUser()
         {
-            var user = await gitHubClient.User.Get("thestamp");
+            var user = await _gitHubClient.User.Get("thestamp");
 
             return user.Login;
         }
+
+        public async Task<RepositoryContent> GetFile(string filename)
+        {
+            var filePath = Path.Combine(_coreSettings.Folder, filename);
+            var fileDetails = await _gitHubClient.Repository.Content.GetAllContentsByRef(
+                _coreSettings.Owner, 
+                _coreSettings.RepoName,
+                filePath, _coreSettings.Branch);
+
+            return fileDetails.First();
+        }
+
+        public async Task<RepositoryContentChangeSet> UpdateFile(string filename, RepositoryContent lastCommit, string newContent, string summary)
+        {
+
+            var updateResult = await _gitHubClient.Repository.Content.UpdateFile(
+                _coreSettings.Owner,
+                _coreSettings.RepoName, 
+                Path.Combine(_coreSettings.Folder, filename),
+                new UpdateFileRequest(summary, newContent, lastCommit.Sha, _coreSettings.Branch));
+
+            return updateResult;
+        }
+
 
     }
 }
--- DataEditor.Web/Program.cs
+++ DataEditor.Web/Program.cs
@@ -1,3 +1,4 @@
+using DataEditor.Core.Configuration;
 using DataEditor.Core.Services;
 using DataEditor.Web;
 using Microsoft.AspNetCore.Components.Web;
@@ -10,6 +11,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
 builder.Services.AddSingleton<GitService>();
+builder.Services.AddSingleton<CoreSettingsModel>(i => new CoreSettingsModel()
+{
+    Branch = "features/devtests",
+    Folder = "csv",
+    Owner = "International-Fairy-Tale-Filmography",
+    RepoName = "data"
+});
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
